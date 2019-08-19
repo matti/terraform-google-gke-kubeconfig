7ea9f407acd506ca62f0572173f9080f98330a7f
Matti Paksula <matti.paksula@iki.fi>
2019-08-19 11:43:03 +0300

    :sunrise:

---
+++ main.tf
@@ -0,0 +1,35 @@
+variable "cluster" {}
+variable "kubeconfig" {}
+
+provider "google" {}
+data "google_client_config" "default" {}
+
+resource "null_resource" "kubeconfig" {
+
+  provisioner "local-exec" {
+    environment = {
+      KUBECONFIG = kubeconfig
+    }
+    command = "gcloud beta container clusters get-credentials ${var.cluster.name} --region ${var.cluster.location} --project ${data.google_client_config.default.project}"
+  }
+}
+
+resource "kubernetes_role_binding" "gke-kube-system-sa-cluster-admin" {
+  depends_on = [null_resource.kubeconfig]
+
+  metadata {
+    name = "gke-kube-system-sa-cluster-admin"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "default"
+    namespace = "kube-system"
+  }
+}
